c23dcb5564544ea6b27294024da55efd932ac0a8
Charles Simpson <charles.simpson@ucl.ac.uk>
2022-05-29 13:40:48 +0100

    Comment out colorjitter

--- src/augmentations.py
+++ src/augmentations.py
@@ -47,25 +47,25 @@ def get_transforms(target_size):
                 JointTransform(Normalize(mean=mean, std=std), None),
             ]
         ),
-        flips_and_colorjitter=JointCompose(
-            [
-                JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
-                JointTransform(
-                    Resize(target_size, Image.BILINEAR), Resize(target_size, Image.NEAREST)
-                ),
-                JointTransform(CenterCrop(target_size), CenterCrop(target_size)),
-                JointRandomRotation(0.5, 90),
-                JointRandomRotation(0.5, 90),
-                JointRandomRotation(0.5, 90),
-                JointRandomHorizontalFlip(0.5),
-                JointRandomVerticalFlip(0.5),
-                JointTransform(
-                    ColorJitter(brightness=0.1, hue=0.1, contrast=0.1, saturation=0.1), None
-                ),
-                JointTransform(ImageToTensor(), MaskToTensor()),
-                JointTransform(Normalize(mean=mean, std=std), None),
-            ]
-        ),
+        #flips_and_colorjitter=JointCompose(
+            #[
+                #JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
+                #JointTransform(
+                    #Resize(target_size, Image.BILINEAR), Resize(target_size, Image.NEAREST)
+                #),
+                #JointTransform(CenterCrop(target_size), CenterCrop(target_size)),
+                #JointRandomRotation(0.5, 90),
+                #JointRandomRotation(0.5, 90),
+                #JointRandomRotation(0.5, 90),
+                #JointRandomHorizontalFlip(0.5),
+                #JointRandomVerticalFlip(0.5),
+                #JointTransform(
+                    #ColorJitter(brightness=0.1, hue=0.1, contrast=0.1, saturation=0.1), None
+                #),
+                #JointTransform(ImageToTensor(), MaskToTensor()),
+                #JointTransform(Normalize(mean=mean, std=std), None),
+            #]
+        #),
         flips_and_fullrotations=JointCompose(
             [
                 JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
