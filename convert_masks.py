358d2e246d7fc421fd2b9270f421ca790236cf91
Charles Simpson <charles.simpson@ucl.ac.uk>
2022-05-29 15:59:29 +0100

    remove redundant code convert_masks.py

--- convert_masks.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from dataset import convert_mask
-from tqdm import tqdm
-
-for s in ("training", "validation", "evaluation"):
-    masks = (str(p) for p in tqdm(list(Path(f"dataset/{s}/labels").glob("*/*/*png"))))
-    convert_mask(masks)
