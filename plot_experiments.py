8d00a20620583ebfd08739240d81be65d9de974e
Charles Simpson <ucbqc38@ucl.ac.uk>
2022-06-16 11:25:17 +0100

    Update plot_experiments.py

--- plot_experiments.py
+++ plot_experiments.py
@@ -6,10 +6,11 @@ import numpy as np
 import pandas as pd
 import toml
 
-paths = Path("/home/ucbqc38/Scratch/experiments").glob("experiment_*")
+paths = list(Path("./").glob("experiment_*"))
 results = []
 best_f1 = 0
 best_config = ""
+# %%
 for p in paths:
     config = toml.load(p / "config.toml")
     with open(p / "history.json", "r") as f:
@@ -19,7 +20,7 @@ for p in paths:
     # continue
     # if config["freeze_pretrained"] == 0:
     # continue
-    label = f"{config['model_path']} ; {config['freeze_pretrained']}; {config['transform']}; {config['lr']}"
+    label = f"{config['freeze_pretrained']}; {config['transform']}; {config['lr']:0.1e}"
     for key in history:
         history[key] = np.array(history[key])
     f_score = history["val tp"] / (
@@ -32,14 +33,16 @@ for p in paths:
     config["miou"] = history["val miou"][-1]
     results.append(config)
     y = 1 - f_score
-    y = f_score_train - f_score
+    # y = f_score_train - f_score
     # y = accuracy_n
     plt.plot(y, label=label)
+
     plt.text(len(y) - 1, y[-1], label)
     plt.yscale("log")
     if f_score[-1] > best_f1:
         best_f1 = f_score[-1]
         best_config = p
+# %%
 
 # df = pd.DataFrame(results).sort_values("f_score")
 # df.to_csv("results.csv")
