3a76f9002e8827c1b5d5273eb8b2220d2011171c
Charles Simpson <ucbqc38@ucl.ac.uk>
2022-06-16 14:58:05 +0100

    Add more augmentations

--- src/augmentations.py
+++ src/augmentations.py
@@ -1,5 +1,11 @@
 from PIL import Image
-from torchvision.transforms import CenterCrop, ColorJitter, Normalize, Resize
+from torchvision.transforms import (
+    CenterCrop,
+    ColorJitter,
+    Normalize,
+    Resize,
+    RandomAdjustSharpness,
+)
 
 from src.transforms import (
     ConvertImageMode,
@@ -49,7 +55,7 @@ def get_transforms(target_size):
                 JointTransform(Normalize(mean=mean, std=std), None),
             ]
         ),
-        flips_and_colorjitter=JointCompose(
+        brightness5=JointCompose(
             [
                 JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
                 JointTransform(
@@ -64,7 +70,7 @@ def get_transforms(target_size):
                 JointRandomVerticalFlip(0.5),
                 JointTransform(
                     ColorJitter(
-                        brightness=0.2,
+                        brightness=0.5,
                     ),
                     None,
                 ),
@@ -72,6 +78,115 @@ def get_transforms(target_size):
                 JointTransform(Normalize(mean=mean, std=std), None),
             ]
         ),
+        hue2=JointCompose(
+            [
+                JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
+                JointTransform(
+                    Resize(target_size, Image.BILINEAR),
+                    Resize(target_size, Image.NEAREST),
+                ),
+                JointTransform(CenterCrop(target_size), CenterCrop(target_size)),
+                JointRandomRotation(0.5, 90),
+                JointRandomRotation(0.5, 90),
+                JointRandomRotation(0.5, 90),
+                JointRandomHorizontalFlip(0.5),
+                JointRandomVerticalFlip(0.5),
+                JointTransform(
+                    ColorJitter(
+                        hue=0.2,
+                    ),
+                    None,
+                ),
+                JointTransform(ImageToTensor(), MaskToTensor()),
+                JointTransform(Normalize(mean=mean, std=std), None),
+            ]
+        ),
+        constrast5=JointCompose(
+            [
+                JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
+                JointTransform(
+                    Resize(target_size, Image.BILINEAR),
+                    Resize(target_size, Image.NEAREST),
+                ),
+                JointTransform(CenterCrop(target_size), CenterCrop(target_size)),
+                JointRandomRotation(0.5, 90),
+                JointRandomRotation(0.5, 90),
+                JointRandomRotation(0.5, 90),
+                JointRandomHorizontalFlip(0.5),
+                JointRandomVerticalFlip(0.5),
+                JointTransform(
+                    ColorJitter(
+                        contrast=0.5,
+                    ),
+                    None,
+                ),
+                JointTransform(ImageToTensor(), MaskToTensor()),
+                JointTransform(Normalize(mean=mean, std=std), None),
+            ]
+        ),
+        blurring5=JointCompose(
+            [
+                JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
+                JointTransform(
+                    Resize(target_size, Image.BILINEAR),
+                    Resize(target_size, Image.NEAREST),
+                ),
+                JointTransform(CenterCrop(target_size), CenterCrop(target_size)),
+                JointRandomRotation(0.5, 90),
+                JointRandomRotation(0.5, 90),
+                JointRandomRotation(0.5, 90),
+                JointRandomHorizontalFlip(0.5),
+                JointRandomVerticalFlip(0.5),
+                JointTransform(
+                    RandomAdjustSharpness(0.5),
+                    None,
+                ),
+                JointTransform(ImageToTensor(), MaskToTensor()),
+                JointTransform(Normalize(mean=mean, std=std), None),
+            ]
+        ),
+        sharpening2=JointCompose(
+            [
+                JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
+                JointTransform(
+                    Resize(target_size, Image.BILINEAR),
+                    Resize(target_size, Image.NEAREST),
+                ),
+                JointTransform(CenterCrop(target_size), CenterCrop(target_size)),
+                JointRandomRotation(0.5, 90),
+                JointRandomRotation(0.5, 90),
+                JointRandomRotation(0.5, 90),
+                JointRandomHorizontalFlip(0.5),
+                JointRandomVerticalFlip(0.5),
+                JointTransform(
+                    RandomAdjustSharpness(2),
+                    None,
+                ),
+                JointTransform(ImageToTensor(), MaskToTensor()),
+                JointTransform(Normalize(mean=mean, std=std), None),
+            ]
+        ),
+        # elastic206=JointCompose(
+        #     [
+        #         JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
+        #         JointTransform(
+        #             Resize(target_size, Image.BILINEAR),
+        #             Resize(target_size, Image.NEAREST),
+        #         ),
+        #         JointTransform(CenterCrop(target_size), CenterCrop(target_size)),
+        #         JointRandomRotation(0.5, 90),
+        #         JointRandomRotation(0.5, 90),
+        #         JointRandomRotation(0.5, 90),
+        #         JointRandomHorizontalFlip(0.5),
+        #         JointRandomVerticalFlip(0.5),
+        #         JointTransform(
+        #             ElasticTransform(alpha=2, sigma=0.06),
+        #             None,
+        #         ),
+        #         JointTransform(ImageToTensor(), MaskToTensor()),
+        #         JointTransform(Normalize(mean=mean, std=std), None),
+        #     ]
+        # ),
         # flips_and_fullrotations=JointCompose(
         #     [
         #         JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
@@ -87,24 +202,24 @@ def get_transforms(target_size):
         #         JointTransform(Normalize(mean=mean, std=std), None),
         #     ]
         # ),
-        flips_and_rotations_and_crops=JointCompose(
-            [
-                JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
-                JointRandomCrop(
-                    (target_size, target_size),
-                    (int(target_size * 0.9), int(target_size * 0.9)),
-                ),
-                JointTransform(
-                    Resize(target_size, Image.BILINEAR),
-                    Resize(target_size, Image.NEAREST),
-                ),
-                JointTransform(CenterCrop(target_size), CenterCrop(target_size)),
-                JointRandomHorizontalFlip(0.5),
-                JointRandomVerticalFlip(0.5),
-                JointFullyRandomRotation(360),
-                JointTransform(ImageToTensor(), MaskToTensor()),
-                JointTransform(Normalize(mean=mean, std=std), None),
-            ]
-        ),
+        # flips_and_rotations_and_crops=JointCompose(
+        #     [
+        #         JointTransform(ConvertImageMode("RGB"), ConvertImageMode("P")),
+        #         JointRandomCrop(
+        #             (target_size, target_size),
+        #             (int(target_size * 0.9), int(target_size * 0.9)),
+        #         ),
+        #         JointTransform(
+        #             Resize(target_size, Image.BILINEAR),
+        #             Resize(target_size, Image.NEAREST),
+        #         ),
+        #         JointTransform(CenterCrop(target_size), CenterCrop(target_size)),
+        #         JointRandomHorizontalFlip(0.5),
+        #         JointRandomVerticalFlip(0.5),
+        #         JointFullyRandomRotation(360),
+        #         JointTransform(ImageToTensor(), MaskToTensor()),
+        #         JointTransform(Normalize(mean=mean, std=std), None),
+        #     ]
+        # ),
     )
     return augs
