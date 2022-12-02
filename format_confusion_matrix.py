d7c9473bad59e1a251aa4cac491a3911f9b17832
Charles Simpson <ucbqc38@ucl.ac.uk>
2022-12-02 16:39:33 +0000

    Group training results

--- format_confusion_matrix.py
+++ format_confusion_matrix.py
@@ -1,61 +1,54 @@
 """
 From already calculated confusion matrix results, make a nicely formatted table.
 """
-import matplotlib.pyplot as plt
+# %%
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv("optimise_postprocessing.csv")
+df = pd.read_csv("confusion_matrix.csv")
+# Join the different training sets together.
+df = pd.concat(
+    (
+        df,
+        df[df.ds.str.contains("training")][["kfold", "tp", "fp", "fn", "tn"]]
+        .groupby("kfold")
+        .sum()
+        .assign(ds="training")
+        .reset_index(),
+    )
+)
+df = df.set_index(["ds", "kfold"]).sort_index()
+
 df["f_score"] = df["tp"] / (df["tp"] + 0.5 * (df["fp"] + df["fn"]))
 df["m"] = df["fp"] + df["fn"] + df["tp"] + df["tn"]
 df["accuracy"] = (df["tp"] + df["tn"]) / df["m"]
 df["precision"] = df.tp / (df.tp + df.fp)
 df["recall"] = df.tp / (df.tp + df.fn)
 df["fpr"] = df.fp / (df.fp + df.tn)
+df["miou"] = np.nanmean(
+    [
+        df.tn / (df.tn + df.fn + df.fp),
+        df.tp / (df.tp + df.fn + df.fp),
+    ],
+    axis=0,
+)
+
+
 for s in ("tp", "fp", "fn", "tn"):
     var = f"{s} / m"
     df[var] = df[s] / df.m
+
 print(
-    df.sort_values("f_score")
-    .tail()[
-        [
-            "accuracy",
-            "precision",
-            "recall",
-            "f_score",
-            "fp / m",
-            "miou",
-            "kernel_size_grow",
-            "kernel_size_denoise",
-        ]
+    df.loc[["training", "validation", "testing"]][
+        ["m", "tp / m", "tn / m", "fp / m", "fn / m"]
     ]
     .round(3)
     .to_latex()
 )
-
-# df.plot.scatter("kernel_size_grow", "f_score")
-df.plot.scatter("kernel_size_denoise", "f_score")
-df.plot.scatter("kernel_size_denoise", "fpr")
-df.plot.scatter("kernel_size_denoise", "recall")
-df.plot.scatter("kernel_size_denoise", "precision")
-df.plot.scatter("kernel_size_grow", "recall")
-df.plot.scatter("kernel_size_grow", "precision")
-plt.show()
-
-
-df = pd.read_csv("confusion_matrix.csv")
-df["f_score"] = df["tp"] / (df["tp"] + 0.5 * (df["fp"] + df["fn"]))
-df["m"] = df["fp"] + df["fn"] + df["tp"] + df["tn"]
-df["accuracy"] = (df["tp"] + df["tn"]) / df["m"]
-df["precision"] = df.tp / (df.tp + df.fp)
-df["recall"] = df.tp / (df.tp + df.fn)
-df["fpr"] = df.fp / (df.fp + df.tn)
-
-
-for s in ("tp", "fp", "fn", "tn"):
-    var = f"{s} / m"
-    df[var] = df[s] / df.m
-
-print(df[["ds", "m", "tp / m", "tn / m", "fp / m", "fn / m"]].round(3).to_latex())
 print(
-    df[["ds", "accuracy", "precision", "recall", "f_score", "miou"]].round(3).to_latex()
+    df.loc[["training", "validation", "testing"]][
+        ["accuracy", "precision", "recall", "f_score", "miou"]
+    ]
+    .round(3)
+    .to_latex()
 )
