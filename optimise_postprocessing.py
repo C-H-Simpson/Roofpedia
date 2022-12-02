afe7770fd37f1c18a5e977ca0c717cfb8c6ecb04
Charles Simpson <ucbqc38@ucl.ac.uk>
2022-12-02 12:02:46 +0000

    Use "best config"

--- optimise_postprocessing.py
+++ optimise_postprocessing.py
@@ -83,13 +83,11 @@ def optimise_postprocessing(loader, num_classes, device, net):
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("config", help="config path")
-    parser.add_argument("checkpoint", help="checkpoint path")
-    args = parser.parse_args()
-    config = toml.load(args.config)
+    config = toml.load("config/best-predict-config.toml")
+    checkpoint_path = Path(config["checkpoint_path"])
+
     # checkpoint_path = "experiment_20220407_151148/Green-checkpoint-100-of-100.pth"
-    chkpt = torch.load(args.checkpoint, map_location=device)
+    chkpt = torch.load(checkpoint_path / "final_checkpoint.pth", map_location=device)
     num_classes = 2
 
     net = UNet(2).to(device)
@@ -100,7 +98,6 @@ if __name__ == "__main__":
     # Optimize the postprocessing
     ds = "validation"
     ds_dir = os.path.join(config["dataset_path"], ds)
-    loader = get_plain_dataset_loader(config["target_size"], 64, ds_dir)
     loader = get_plain_dataset_loader(config["target_size"], 64, ds_dir)
     tile_size = config["target_size"]
     opt = optimise_postprocessing(loader, num_classes, device, net)
