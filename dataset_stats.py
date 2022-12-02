066b7b30d88d72c9e6ccb628a393b5e38d0a43cb
Charles Simpson <ucbqc38@ucl.ac.uk>
2022-12-02 09:57:22 +0000

    Allow to check all datasets

--- dataset_stats.py
+++ dataset_stats.py
@@ -1,22 +1,27 @@
-# %%
+"""
+Check the amount of positive and negative pixels / examples in each dataset.
+"""
 from pathlib import Path
 
 import cv2
 import numpy as np
-from tqdm import tqdm
 
-for s in ("training", "validation", "evaluation"):
-    masks = list(Path(f"dataset/{s}/labels").glob("*/*/*png"))
-    not_blank = [np.count_nonzero(cv2.imread(str(p)) > 200) for p in tqdm(masks)]
-    count_not_blank = np.sum(not_blank) # number of non-blank pixels
-    all_not_blank = np.count_nonzero(not_blank) # number of non blank tiles
-    n = len(masks)
-    print(f"{s}: all {n}, all pix {all_pix}, not blank {count_not_blank}, all not blank {all_not_blank}")
-    pos_wt = 1-count_not_blank/(all_not_blank*2)/(256**2)
-    print(f"pos wt = {pos_wt:0.2e}, neg_wt = {1-pos_wt:0.2e}")
+dataset_parents = Path("dataset").glob("*")
+for p in dataset_parents:
+    for s in ("training", "validation", "evaluation"):
+        if not (p / s).is_dir():
+            # print(p / s, "not a dir")
+            continue
+        masks = list((p / s / "labels").glob("*/*/*png"))
+        not_blank = [np.count_nonzero(cv2.imread(str(p)) > 200) for p in (masks)]
+        count_not_blank = np.sum(not_blank)  # number of non-blank pixels
+        all_not_blank = np.count_nonzero(not_blank)  # number of non blank tiles
+        n = len(masks)
+        print(p / s)
+        print(f"\t{n=}, {count_not_blank=}, {all_not_blank=}")
+        pos_wt = 1 - count_not_blank / (all_not_blank * 2) / (256**2)
+        print(f"\tpos wt = {pos_wt:0.2e}, neg_wt = {1-pos_wt:0.2e}")
 
-    # img = cv2.imread(str(masks[0]))
-    # print(img)
-    # breakpoint()
-
-# %%
+        # img = cv2.imread(str(masks[0]))
+        # print(img)
+        # breakpoint()
