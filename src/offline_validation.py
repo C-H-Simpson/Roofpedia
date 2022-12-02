1987ac18e52a78a2870f326f0fde11f0eede2aed
Charles Simpson <ucbqc38@ucl.ac.uk>
2022-12-02 13:57:34 +0000

    Debug
    Division by zero
    Bad iteration over glob generator

--- src/offline_validation.py
+++ src/offline_validation.py
@@ -19,15 +19,12 @@ from src.plain_dataloader import get_plain_dataset_loader
 from src.unet import UNet
 
 
-def validate_offline(loader, num_classes, device, net, use_postprocess=False):
+def validate_offline(loader, num_classes, device, net):
     """This version of the validate routine can be used to test postprocess.
     Also, can take different loaders as input."""
-    # kernel_size_denoise = 1
-    # kernel_size_grow = 9
     num_samples = 0
 
     metrics = Metrics(range(num_classes))
-    # metrics_postprocess = Metrics(range(num_classes))
 
     with torch.no_grad():
         net.eval()
@@ -56,26 +53,13 @@ def validate_offline(loader, num_classes, device, net, use_postprocess=False):
             "tp": metrics.tp,
             "fn": metrics.fn,
             "tn": metrics.tn,
-            "tp+fn" : metrics.tp+metrics.fn,
-            "f1": metrics.tp / (metrics.tp + 0.5 * (metrics.fp + metrics.fn)),
+            "tp+fn": metrics.tp + metrics.fn,
         }
-        # if use_postprocess:
-        #     result_postprocess = {
-        #         "miou": metrics_postprocess.get_miou(),
-        #         "fg_iou": metrics_postprocess.get_fg_iou(),
-        #         "mcc": metrics_postprocess.get_mcc(),
-        #         "fp": metrics_postprocess.fp,
-        #         "tp": metrics_postprocess.tp,
-        #         "fn": metrics_postprocess.fn,
-        #         "tn": metrics_postprocess.tn,
-        #     }
-        # else:
-        #     result_postprocess = {}
-
-        return {
-            "raw": result,
-            # "postprocess": result_postprocess
-        }
+        try:
+            result["f1"] = metrics.tp / (metrics.tp + 0.5 * (metrics.fp + metrics.fn))
+        except ZeroDivisionError:
+            result["f1"] = float("NAN")
+        return result
 
 
 if __name__ == "__main__":
@@ -87,11 +71,11 @@ if __name__ == "__main__":
 
     # We will iterate over the "best config" and its kfolds
     original_config = toml.load("config/best-predict-config.toml")
-    kfold_config_paths = Path("results").glob("kfold*")
+    kfold_config_paths = list(Path("results").glob("kfold*/config.toml"))
 
     # Check the kfold files match the original config.
     for p in kfold_config_paths:
-        config = toml.load(p / "config.toml")
+        config = toml.load(p)
         for key in original_config:
             if key in ("dataset_path", "checkpoint_path", "kfold"):
                 continue
@@ -101,15 +85,12 @@ if __name__ == "__main__":
                     + f"{p} {key} {config[key]} != {original_config[key]}"
                 )
 
-        print(f"{config['kfold']=}")
-
-    results = []
-    results_postprocess = []
-
     # Iterate through k folds and do offline validation.
-    for config in [original_config] + [
-        toml.load(p / "config.toml") for p in kfold_config_paths
-    ]:
+    results = []
+    kfold_config_paths.append("config/best-predict-config.toml")
+    for config in kfold_config_paths:
+        print(config)
+        config = toml.load(config)
         chkpt_path = Path(config["checkpoint_path"]) / "final_checkpoint.pth"
         chkpt = torch.load(chkpt_path, map_location=device)
         num_classes = 2
@@ -129,19 +110,12 @@ if __name__ == "__main__":
             loader = get_plain_dataset_loader(config["target_size"], batch_size, ds_dir)
             tile_size = config["target_size"]
 
-            val = validate_offline(loader, num_classes, device, net, True)
-            # val["raw"]["ds"] = ds
-            # val["raw"]["postprocess"] = False
-            # val["postprocess"]["ds"] = ds
-            # val["postprocess"]["postprocess"] = True
-            results.append(val["raw"])
-            # results_postprocess.append(val["postprocess"])
-        break
+            val = validate_offline(loader, num_classes, device, net)
+            val["kfold"] = config["kfold"]
+            val["ds"] = ds
+            results.append(val)
 
-    df = pd.DataFrame(
-        results
-        # + results_postprocess
-    )
+    df = pd.DataFrame(results)
     df.to_csv("confusion_matrix.csv")
     print(df)
     print("wrote to confusion_matrix.csv")
