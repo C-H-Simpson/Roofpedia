6247648eb01ec9cdad85ea649fab2cd2c9b81503
Charles Simpson <ucbqc38@ucl.ac.uk>
2022-12-02 09:48:38 +0000

    Save evaluation stats too

--- kfold_testing.py
+++ kfold_testing.py
@@ -1,6 +1,5 @@
 """
-In this script, we repeat training across multiple folds,
-and produce performance statistics.
+Repeat training across multiple folds, and produce performance statistics.
 """
 
 import collections
@@ -21,6 +20,7 @@ from src.losses import CrossEntropyLoss2d, FocalLoss2d, LovaszLoss2d, mIoULoss2d
 from src.train import get_dataset_loaders, train, validate
 from src.unet import UNet
 from src.utils import plot
+from src.plain_dataloader import get_plain_dataset_loader
 
 
 def run_training():
@@ -69,6 +69,9 @@ def run_training():
     train_loader, val_loader = get_dataset_loaders(
         target_size, batch_size, dataset_path, signal_fraction, augs[transform_name]
     )
+    eval_loader = get_plain_dataset_loader(
+        target_size, batch_size, Path(dataset_path) / "evaluation"
+    )
     history = collections.defaultdict(list)
 
     # training loop
@@ -76,24 +79,25 @@ def run_training():
 
         print("Epoch: " + str(epoch + 1))
         train_hist = train(train_loader, num_classes, device, net, optimizer, criterion)
-
         val_hist = validate(val_loader, num_classes, device, net, criterion)
+        eval_hist = validate(eval_loader, num_classes, device, net, criterion)
 
         print(
             "Train stats:",
             ", ".join([f"{key}: {train_hist[key]:.3e}" for key in train_hist]),
         )
-
         print(
             "Validation stats:",
             ", ".join([f"{key}: {val_hist[key]:.3e}" for key in val_hist]),
         )
 
-        for key, value in train_hist.items():
-            history["train " + key].append(value)
-
-        for key, value in val_hist.items():
-            history["val " + key].append(value)
+        for label, hist in (
+            ("train", train_hist),
+            ("val", val_hist),
+            ("eval", eval_hist),
+        ):
+            for key, value in hist.items():
+                history[label + key].append(value)
 
         with open(checkpoint_path / "history.json", "w") as f:
             json.dump(history, f)
@@ -120,7 +124,7 @@ def run_training():
 
 
 if __name__ == "__main__":
-    config = toml.load("config/train-config.toml")
+    config = toml.load("config/best-predict-config.toml")
 
     num_classes = 2
     lr = config["lr"]
@@ -147,6 +151,8 @@ if __name__ == "__main__":
         # make dir for checkpoint - will get moved
         checkpoint_path.mkdir(exist_ok=True)
         dataset_path = f"dataset/k{k}"
+        fname = f"kfold_{k}_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        config["checkpoint_path"] = fname
         # Write the testing config to file
         with open(checkpoint_path / "config.toml", "w") as f:
             toml.dump(config, f)
@@ -154,7 +160,4 @@ if __name__ == "__main__":
         run_training()
 
         # Move the config and results to a new directory
-        fname = f"kfold_training_{k}" + datetime.datetime.now().strftime(
-            "%Y%m%d_%H%M%S"
-        )
         shutil.move(checkpoint_path, fname)
