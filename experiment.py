56b728b381c94544ed3628bfcc5a06a185b6c636
Charles Simpson <ucbqc38@ucl.ac.uk>
2022-12-12 16:27:53 +0000

    log alternative validation performance

--- experiment.py
+++ experiment.py
@@ -27,10 +27,12 @@ from dataset_stats import get_signal_weight
 from src.train import get_dataset_loaders, train, validate
 from src.unet import UNet
 from src.utils import plot
+from src.plain_dataloader import get_plain_dataset_loader
 
 
 def run_training(
     *,
+    alt_validation_path,
     augs,
     batch_size,
     checkpoint_path,
@@ -88,6 +90,15 @@ def run_training(
     train_loader, val_loader = get_dataset_loaders(
         target_size, batch_size, dataset_path, signal_fraction, augs[transform_name]
     )
+    alt_validation_path = Path(alt_validation_path)
+    if alt_validation_path.is_dir():
+        alt_val_loader = get_plain_dataset_loader(
+            target_size, batch_size, alt_validation_path
+        )
+    else:
+        print("Alternative validation data not available", alt_validation_path)
+        alt_val_loader = None
+
     history = collections.defaultdict(list)
 
     # training loop
@@ -98,6 +109,9 @@ def run_training(
 
         val_hist = validate(val_loader, num_classes, device, net, criterion)
 
+        if alt_val_loader:
+            alt_val_hist = validate(alt_val_loader, num_classes, device, net, criterion)
+
         print(
             "Train stats:",
             ", ".join([f"{key}: {train_hist[key]:.3e}" for key in train_hist]),
@@ -107,12 +121,21 @@ def run_training(
             "Validation stats:",
             ", ".join([f"{key}: {val_hist[key]:.3e}" for key in val_hist]),
         )
+        if alt_val_loader:
+            print(
+                "Alt validation stats:",
+                ", ".join([f"{key}: {alt_val_hist[key]:.3e}" for key in alt_val_hist]),
+            )
 
         for key, value in train_hist.items():
             history["train " + key].append(value)
 
         for key, value in val_hist.items():
             history["val " + key].append(value)
+
+        if alt_val_loader:
+            for key, value in alt_val_hist.items():
+                history["alt val " + key].append(value)
 
         with open(checkpoint_path / "history.json", "w") as f:
             json.dump(history, f)
@@ -155,6 +178,8 @@ if __name__ == "__main__":
     batch_size = config["batch_size"]
 
     dataset_path = config["dataset_path"]
+    alt_validation_path = str((Path(dataset_path) / "alt_validation").resolve())
+    config["alt_validation"] = alt_validation_path
     checkpoint_path = Path(config["checkpoint_path"])
     target_type = config["target_type"]
     signal_fraction = config["signal_fraction"]
@@ -208,6 +233,7 @@ if __name__ == "__main__":
                     f.write(toml.dumps(config))
 
                 run_training(
+                    alt_validation_path=alt_validation_path,
                     augs=augs,
                     batch_size=batch_size,
                     checkpoint_path=checkpoint_path,
@@ -224,89 +250,3 @@ if __name__ == "__main__":
                     weight=weight,
                     focal_gamma=focal_gamma,
                 )
-
-# # %%
-# # Single experiment
-# if __name__ == "__main__":
-#     config = toml.load("config/best-predict-config.toml")
-
-#     num_classes = 2
-#     loss_func = config["loss_func"]
-#     num_epochs = config["num_epochs"]
-#     target_size = config["target_size"]
-#     batch_size = config["batch_size"]
-
-#     dataset_path = config["dataset_path"]
-#     checkpoint_path = Path(config["checkpoint_path"])
-#     target_type = config["target_type"]
-#     signal_fraction = config["signal_fraction"]
-#     # weight = [float(signal_fraction), 1.0]
-#     # config["weight"] = weight
-#     config["early_stopping"] = "val loss"
-
-#     freeze_pretrained = True
-#     config["freeze_pretrained"] = freeze_pretrained
-
-#     loss_func = "CrossEntropy"
-#     config["loss_func"] = loss_func
-
-#     weight = [
-#         9.52e-02,  # negative class is big therefore small weight
-#         9.05e-01,  # positive class is small therefore big weight
-#     ]
-#     config["weight"] = weight
-
-#     focal_gamma = None  # 4
-#     config["focal_gamma"] = focal_gamma
-
-#     lr = config["lr"]
-#     lr = 5e-5
-#     config["lr"] = 5e-4
-
-#     # if config["model_path"] != "":
-#     # model_path = config["model_path"]
-#     # else:
-#     # model_path = None
-
-#     transform_name = "persp"
-#     config["transform"] = transform_name
-#     # transform_name = config["transform"]
-
-#     Path("results").mkdir(exist_ok=True)
-
-#     augs = get_transforms(target_size)
-#     # Training a model from scratch
-#     config["model_path"] = ""
-#     model_path = ""
-#     # make dir for checkpoint
-#     fname = "results/experiment_" + datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-#     checkpoint_path = Path(fname)
-#     checkpoint_path.mkdir(exist_ok=False)
-#     config["checkpoint_path"] = fname
-#     # Write the testing config to file
-#     with open(checkpoint_path / "config.toml", "w") as f:
-#         f.write(toml.dumps(config))
-
-#     run_training(
-#         augs=augs,
-#         batch_size=batch_size,
-#         checkpoint_path=checkpoint_path,
-#         dataset_path=dataset_path,
-#         freeze_pretrained=freeze_pretrained,
-#         loss_func=loss_func,
-#         lr=lr,
-#         model_path=model_path,
-#         num_classes=num_classes,
-#         num_epochs=num_epochs,
-#         signal_fraction=signal_fraction,
-#         target_size=target_size,
-#         transform_name=transform_name,
-#         weight=weight,
-#         focal_gamma=focal_gamma,
-#     )
-
-# # Move the config and results to a new directory
-# # No necessary as it should have been written in the right place!
-# # shutil.move(checkpoint_path, fname)
-
-# # %%
