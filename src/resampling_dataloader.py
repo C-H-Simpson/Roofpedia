8a33d68073214356e6decf4ffbbfd97f9eb7638b
Charles Simpson <ucbqc38@ucl.ac.uk>
2022-12-16 11:11:15 +0000

    Include both ideas for resampling

--- src/resampling_dataloader.py
+++ src/resampling_dataloader.py
@@ -39,3 +39,33 @@ class BackgroundResamplingLoader(Dataset):
         else:
             idx = (i - self.n_background) % self.n_signal
             return self.signal_tiles[idx]
+
+
+class SignalResamplingLoader(Dataset):
+    """Do resampling with replacement, compensating for class imbalance."""
+
+    def __init__(self, signal_tiles, background_tiles, signal_p=0.5):
+        self.signal_p = signal_p
+        self.signal_tiles = signal_tiles
+        self.background_tiles = background_tiles
+
+        self.n_signal = len(self.signal_tiles)
+        self.n_background = len(self.background_tiles)
+        if signal_p == 1:
+            self.length = self.n_signal
+            self.n_background = 0
+        elif signal_p == -1:
+            self.length = self.n_background + self.n_signal
+        else:
+            assert (signal_p > 0) and (signal_p < 1)
+            self.length = int(self.n_background / (1 - signal_p))
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, i):
+        if i < self.n_background:
+            self.background_tiles[i]
+        else:
+            idx = (i - self.n_background) % self.n_signal
+            return self.signal_tiles[idx]
