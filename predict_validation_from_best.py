58b62edf339f9adc43038ff5e5b7384c3c9deac7
Charles Simpson <ucbqc38@ucl.ac.uk>
2023-01-04 10:26:03 +0000

    Update predict_validation_from_best.py

--- predict_validation_from_best.py
+++ predict_validation_from_best.py
@@ -6,9 +6,12 @@ import toml
 import torch
 import geopandas as gpd
 import pandas as pd
+import pygeos
 
 from src.extract import extract
 from src.predict import predict
+
+gpd.options.use_pygeos = True
 
 # from imagery_tiling.batched_tiling import tiling_path, native_crs
 tiling_path = "./tiling_256_0.25.feather"
@@ -30,9 +33,12 @@ chkpt = torch.load(
 truth_path = Path(
     r"C:\Users\ucbqc38\Documents\RoofPedia\gr_manual_labels_221212.geojson"
 )
+truth = gpd.read_file(truth_path).to_crs(native_crs)
+truth = gpd.GeoDataFrame(geometry=truth.geometry.explode(index_parts=False), crs=truth.crs) # Fix self intersection which is my fault.
+truth.to_file("truth_exploded.geojson")
 
-# for name in ("validation", "training_s", "training_b",):
-for name in ("training_b",):
+for name in ("validation", "training_s", "training_b",):
+    print(f"dataset={name}")
 
     tiles_dir = Path(config["dataset_path"]) / name
     mask_dir = Path("results") / name / "masks"
@@ -46,6 +52,7 @@ for name in ("training_b",):
     input_glob = list(mask_dir.glob("*/*png"))
     polygon_output_path = mask_dir.parent / f"{name}.geojson"
 
+    print("Extraction")
     extract(
         input_glob,
         polygon_output_path,
@@ -64,14 +71,22 @@ for name in ("training_b",):
         .to_crs(native_crs)
         .reset_index()
     )
-    truth = gpd.read_file(truth_path).to_crs(native_crs)
     print(predictions.crs, truth.crs, gdf_tiles.crs)
 
-    truth = gpd.overlay(truth, gdf_tiles, "intersection")
-
-    if not truth.empty:
-        fp = gpd.overlay(predictions, truth, "difference")
-    else:
+    print("Truth overlay")
+    try:
+        truth_local = gpd.overlay(truth, gdf_tiles, "intersection")
+        truth_local.to_file(polygon_output_path.parent / "truth_local.geojson", driver="GeoJSON")
+        print("gen fp")
+        fp = gpd.overlay(predictions, truth_local, "difference")
+        print("gen fn")
+        fn = gpd.overlay(truth_local, predictions, "difference")
+        if not fn.empty:
+            fn.to_file(polygon_output_path.parent / "fn.geojson", driver="GeoJSON")
+        else:
+            print("No false negatives?")
+    except:
+        print("No truth geometry")
         fp = predictions
 
     if not fp.empty:
@@ -79,15 +94,29 @@ for name in ("training_b",):
     else:
         print("No false positives?")
 
-    if not truth.empty:
-        fn = gpd.overlay(truth, predictions, "difference")
-        fn.to_file(polygon_output_path.parent / "fn.geojson", driver="GeoJSON")
-    else:
-        print("No false negatives?")
 
-pd.concat((gpd.read_file(p) for p in Path("results").glob("*/fp.geojson"))).to_file(
-    "merged_fp.geojson"
+# %%
+print("Merging fp")
+fp = list(Path("results").glob("*/fp.geojson"))
+if not fp:
+    print("No fp?")
+else:
+    pd.concat((gpd.read_file(p) for p in fp)).to_file(
+        "merged_fp.geojson"
+    )
+print("Merging fn")
+fn = list(Path("results").glob("*/fn.geojson"))
+if not fn:
+    print("No fn?")
+else:
+    pd.concat((gpd.read_file(p) for p in fn)).to_file(
+        "merged_fn.geojson"
+    )
+
+all_pred = ["results/training_b/training_b.geojson", "results/validation/validation.geojson", "results/training_s/training_s.geojson"]
+pd.concat((gpd.read_file(p) for p in all_pred)).to_file(
+    "merged_pred.geojson"
 )
-pd.concat((gpd.read_file(p) for p in Path("results").glob("*/fn.geojson"))).to_file(
-    "merged_fn.geojson"
-)
+
+# %%
+# It seems like there might be a crs issue again? There are no false negatives, and the false positives file is very large.
